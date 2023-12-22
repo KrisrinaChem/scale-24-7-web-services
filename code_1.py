12bb176cff4ef4ffe2df7ef052ded31722700b14
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-12-22 18:34:55 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,41 @@
+        }
+public class RandomDataGenerator {
+    }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+
+        }
+
+}            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+
+    public static void main(String[] args) {
+        }
+            System.out.println("Random Number: " + item);
+        }
+        int[] data = new int[10];
+public class RandomDataGenerator {
+
+}    return data;
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    });
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+}
+}
+function main() {
+
+
+    }
+    data.forEach(item => {
+main();
