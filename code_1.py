94674c878e1172fdca0f64c85e3b9aab4e76d38b
Krisrina Chem <140569870+KrisrinaChem@users.noreply.github.com>
2023-12-25 13:31:58 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,41 +1,25 @@
-        }
+
+        for (int i = 0; i < 10; i++) {
 public class RandomDataGenerator {
-    }
     public static void main(String[] args) {
             data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
-
-        }
-
-}            data[i] = (int) (Math.random() * 100) + 1;
-    }
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-
-    public static void main(String[] args) {
         }
             System.out.println("Random Number: " + item);
-        }
-        int[] data = new int[10];
-public class RandomDataGenerator {
-
-}    return data;
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-function generateRandomData() {
-    });
-        console.log("Random Number: " + item);
-    const data = generateRandomData();
+        for (int item : data) {
 }
-}
-function main() {
-
 
     }
-    data.forEach(item => {
-main();
+        int[] data = new int[10];
+        }if __name__ == "__main__":
+    data = generate_random_data()
+    main()
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    return data
+def main():
+import random
+
+
+
+        print(f"Random Number: {item}")
